5296462cb09fbb8abf21e2c91859b7301776d908
Jordan <jordanboardman15@gmail.com>
2022-08-10 10:32:44 -0400

    test push

---
+++ test.py
@@ -0,0 +1,39 @@
+#1. Hello You!
+
+# Prompt the user for his name using the input function. Example:
+# name = input('What is your name? ')
+
+# Upon receiving their name, you will say hello to them. Example session:
+
+# $ python3 hello.py
+# What is your name? Alice
+# Hello, Alice!
+
+# name = input('What is your name? ')
+
+# print('Hello', (name))
+
+# ----------------------------
+
+# 2. HELLO YOU!
+# Same as the first exercise, but this time print the user's name in ALL CAPS, and also tell them the number of letters in their name.
+
+# (Hint: You'll want to search for documentation on how to uppercase a string.)
+
+# Example session:
+
+# $ python3 hello2.py
+# WHAT IS YOUR NAME? Alice
+# HELLO, ALICE!
+# YOUR NAME HAS 5 LETTERS IN IT! AWESOME!
+
+name = input('WHAT IS YOUR NAME? ')
+
+print("HELLO,",(name.upper()),"!")
+print("YOUR NAME HAS",(len(name)),"LETTERS IN IT! AWESOME!")
+
+#
+
+
+
+    
