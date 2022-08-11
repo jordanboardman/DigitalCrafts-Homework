aa56e1973be96b2de23f032213ebd4f9c3f07e97
Jordan <jordanboardman15@gmail.com>
2022-08-11 08:50:23 -0400

    created reverseString.py

---
+++ interviewproblems/reverseString.py
@@ -0,0 +1,8 @@
+# # # Exercise: Given a string, create a function that returns the string reversed. (edited) 
+
+def gamefunc(x):
+    return x[::-1]
+
+my_favorite_game = gamefunc('Donkey Kong Country')
+
+print(my_favorite_game)
