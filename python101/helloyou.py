359388910e262782cf531797a03035f044749aaa
Jordan <jordanboardman15@gmail.com>
2022-08-11 08:31:34 -0400

    created helloyou.py file

---
+++ python101/helloyou.py
@@ -0,0 +1,14 @@
+# 1. Hello You!
+
+# Prompt the user for his name using the input function. Example:
+# name = input('What is your name? ')
+
+# Upon receiving their name, you will say hello to them. Example session:
+
+# $ python3 hello.py
+# What is your name? Alice
+# Hello, Alice!
+
+name = input('What is your name? ')
+
+print('Hello', (name))
