7ca110d20b0392dc86c0791b48f35b581914fd36
Jordan <jordanboardman15@gmail.com>
2022-08-11 11:45:21 -0400

    created highScore.py

---
+++ interviewproblems/python/highScore.py
@@ -0,0 +1,16 @@
+#Not complete
+
+# # 8 players
+# # Given a key/value structure, write a function that returns the top three players with the highest score, each being assigned with a medal.
+
+# #Ex: {'Kate': 12, 'Bill': 23, 'Joe': 9, 'Jackie': 22, 'Lin': 18}
+# #    {'Gold': 'Bill', 'Silver': 'Jackie', 'Bronze': 'Lin'}
+
+# players = {'Jack': 10, 'Jill': 23, 'Monty': 45, 'Norman': 50, 'Brodi': 75, "Bob": 86, 'Jay': 60, 'Banksy': 73}
+
+# firstPlace = max
+
+    
+
+
+# print(firstPlace)
