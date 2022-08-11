eabe554ba1dc71519794ef6a7ae96bc12a01b29a
Jordan <jordanboardman15@gmail.com>
2022-08-11 08:33:30 -0400

    created helloyou2.py file

---
+++ python101/helloyou2.py
@@ -0,0 +1,20 @@
+# 2. HELLO YOU!
+# Same as the first exercise, but this time print the user's name in ALL CAPS, and also tell them the number of letters in their name.
+
+# (Hint: You'll want to search for documentation on how to uppercase a string.)
+
+# Example session:
+
+# $ python3 hello2.py
+# WHAT IS YOUR NAME? Alice
+# HELLO, ALICE!
+# YOUR NAME HAS 5 LETTERS IN IT! AWESOME!
+
+name = input('WHAT IS YOUR NAME? ')
+
+print("HELLO,",(name.upper()),"!")
+print("YOUR NAME HAS",(len(name)),"LETTERS IN IT! AWESOME!")
+
+#
+
+print('Hello world')
