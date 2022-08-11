be1bd5006e7bbcccd2aaba678bb642c91cb590cb
Jordan <jordanboardman15@gmail.com>
2022-08-11 08:37:14 -0400

    moved interviewproblems folder w/files to Homework repo

---
+++ interviewproblems/interviewproblems.py
@@ -0,0 +1,69 @@
+#-----------
+
+# Write a program to check if the given number is a palindrome number.
+# A palindrome number is a number that is same after reverse. For example 545, is the palindrome numbers
+
+# userNum = (input('Please type in a palindrome number: '))
+# reversedInput = (userNum[::-1])
+
+# # for x in userNum:
+# if userNum == reversedInput:
+#     print('%s is a palindrome' % userNum)
+# else:
+#     print('%s is not a palindrome' % userNum)
+
+# ---
+
+# #numbers = (input('Please enter a number that has at least three characters: '))
+
+# try:
+#     checkInt = int(numbers)
+#     if len(numbers) > 2:
+#         palindrome = numbers[::-1]
+#         if numbers == palindrome:
+#             print('The number %s is a palindrome!' % numbers)
+#         else:
+#             print('The number %s is not a palindrome!' % numbers)
+#     else:
+#         print('Invalid entry.')
+# except:
+#     print('Invalid entry.')
+
+# Haven't complete for non-numbers input
+
+#---
+
+# word = input("Enter a word. ")
+# half = len(word)/2
+# half = int(half)
+# print(word[0:half+1])
+# print(word[len(word):half-1:-1])
+
+# if word[0:half+1] == word[len(word):half-1:-1]:
+#     print("It is a palindrome. ")
+# else:
+#     print("Not a palindrome. ")
+
+
+
+
+
+
+
+# Write a short program that prints each number from 1 to 100 on a new line. 
+
+# For each multiple of 3, print "Fizz" instead of the number. 
+
+# For each multiple of 5, print "Buzz" instead of the number. 
+
+# For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
+
+# for x in range(1, 101):
+# #     if x % 3 == 0 and x % 5 == 0:
+# #         print('FizzBuzz')
+# #     elif x % 3 == 0:
+# #         print('Fizz')
+# #     elif x % 5 == 0:
+# #         print('Buzz')
+# #     else: 
+#         # print(x)
