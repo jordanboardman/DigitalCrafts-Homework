72c66c69221ff4fc027b6a0ce97e5190abe9b585
Jordan <jordanboardman15@gmail.com>
2022-08-11 08:39:04 -0400

    added fizzbuzz.py to interviewproblems

---
+++ interviewproblems/fizzbuzz.py
@@ -0,0 +1,17 @@
+# Write a short program that prints each number from 1 to 100 on a new line. 
+
+# For each multiple of 3, print "Fizz" instead of the number. 
+
+# For each multiple of 5, print "Buzz" instead of the number. 
+
+# For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
+
+for x in range(1, 101):
+    if x % 3 == 0 and x % 5 == 0:
+        print('FizzBuzz')
+    elif x % 3 == 0:
+        print('Fizz')
+    elif x % 5 == 0:
+        print('Buzz')
+    else: 
+        print(x)
