193815e60df5c547edb06908ec317891ea035cf8
Jordan <jordanboardman15@gmail.com>
2022-08-11 08:54:27 -0400

    created anagram.py

---
+++ interviewproblems/anagram.py
@@ -0,0 +1,12 @@
+# When given two strings for the user, write a function that checks if each string is an anagram of the other. If yes, return true, otherwise false.
+
+#Ex: Debit Card -> Bad Credit
+#    Listen -> Silent
+
+s1 = input('Please enter a word: ')
+s2 = input('Please enter another word: ')
+
+if (Counter(s1) == Counter(s2)):
+        print("Whoa! it's an anagram.")
+else:
+        print("Bummer! No anagram.")
