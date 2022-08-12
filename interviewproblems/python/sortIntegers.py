e3edcabde56f373596c2b3796b2a694fbbb0905b
Jordan <jordanboardman15@gmail.com>
2022-08-12 11:28:34 -0400

    created file

---
+++ interviewproblems/python/sortIntegers.py
@@ -0,0 +1,10 @@
+#Without using the built in python library, sort an array of integers from least to greatest.
+
+# Ex: [423, 53123, 5, 892, 94, 23453, 75, 2725, 64, 904]
+
+num = [423, 53123, 5, 892, 94, 23453, 75, 2725, 64, 904]
+sortNum =[]
+
+while num:
+    minimum = num[0]
+    
