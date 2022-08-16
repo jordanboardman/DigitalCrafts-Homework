6c068e95ff9c585316c76ba0504b886f5eea9b02
Jordan <jordanboardman15@gmail.com>
2022-08-16 12:52:58 -0400

    name changes and dogShelter

---
+++ interviewproblems/python/dogShelter.py
@@ -0,0 +1,33 @@
+#(NOT COMPLETE)
+# #brown fur or weighs over 50 lbs -> new shelter.
+
+#Ex. [{breed: German Shepard, weight: 88, fur(brown, black)}, {breed: Golden Retriever, weight: 45, fur: tan}, {breed: Terrier, weight: 29, fur: black}, {breed French Bulldog, weight: 12, fur: gray}]
+
+# def function that returns the above result. 
+# create list of categories?
+
+
+dogs = [{
+    "breed": "German Shepard",
+    "weight": 88,
+    "fur": ("brown", "black")
+},
+    {
+    "breed": "Golden Retriever",
+    "weight": 45,
+    "fur": "tan",
+},
+    {
+    "breed": "Terrier",
+    "weight": 29,
+    "fur": "black",
+},
+    {
+    "breed": "French Bulldog",
+    "weight": 12,
+    "fur": "gray"
+},]
+
+newCenter = [{}]
+
+
