a53a0692d7fba5dc39592abafa8cefb2eae014bf
Jordan <jordanboardman15@gmail.com>
2022-08-17 12:04:58 -0400

    made morseCode

---
+++ interviewproblems/python/morseCode.py
@@ -0,0 +1,71 @@
+#(NOT COMPLETED)
+
+# International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows:
+# 'a' maps to ".-",
+# 'b' maps to "-...",
+# 'c' maps to "-.-.", and so on.
+
+# For convenience, the full table for the 26 letters of the English alphabet is given below:
+# [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+
+# Given an array of strings words where each word can be written as a concatenation of the Morse code of each letter.
+# For example, "cab" can be written as "-.-..--...", which is the concatenation of "-.-.", ".-", and "-...". We will call such a concatenation the transformation of a word.
+
+# Return the number of different transformations among all words we have.
+
+# Example 1:
+# Input: words = ["gin","zen","gig","msg"]
+# Output: 2
+# Explanation: The transformation of each word is:
+# "gin" -> "--...-."
+# "zen" -> "--...-."
+# "gig" -> "--...--."
+# "msg" -> "--...--."
+# There are 2 different transformations: "--...-." and "--...--.".
+
+# def morse(words: list) -> int
+
+# Write a function that takes a list of words and return the number of unique transformations
+
+from operator import indexOf
+
+
+letters = {
+        "a": ".-",
+        "b":  "-...",
+        "c":   "-.-.",
+        "d": "-..",
+        "e": ".",
+        "f": "..-.",
+        "g":  "--.",
+        "h": "....",
+        "i": "..",
+        "j": ".---",
+        "k": "-.-",
+        "l": ".-..",
+        "m": "--",
+        "n": "-.",
+        "o": "---",
+        "p": ".--.",
+        "q": "--.-",
+        "r": ".-.",
+        "s": "...",
+        "t": "-",
+        "u": "..-",
+        "v": "...-",
+        "w": ".--",
+        "x": "-..-",
+        "y": "-.--",
+        "z": "--.."
+}
+
+words = ["guy", "dog", "ran", "jog"]
+
+wordGuy = letters["g"+"u"+"y"]
+wordDog = letters["d"+"o"+"g"]
+wordRan = letters["r"+"a"+"n"]
+wordJog = letters["j"+"o"+"g"]
+
+
+
+
