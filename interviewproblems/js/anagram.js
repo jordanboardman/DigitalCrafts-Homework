2b246046d50eec413b85083e8808810e1e5cd8d2
Jordan <jordanboardman15@gmail.com>
2022-08-22 16:54:46 -0400

    saved

--- interviewproblems/js/anagram.js
+++ interviewproblems/js/anagram.js
@@ -1,4 +1,4 @@
-// When given two strings from the user, write a function that checks if each string is an anagram of the other. If yes, return true, otherwise false
+// // When given two strings from the user, write a function that checks if each string is an anagram of the other. If yes, return true, otherwise false
 
-prompt("please enter a word");
-console.log(prompt);
+// prompt("please enter a word");
+// console.log(prompt);
