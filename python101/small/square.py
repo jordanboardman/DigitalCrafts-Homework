45f76eda5674d733d7753149da8908490e570c8e
Jordan <jordanboardman15@gmail.com>
2022-08-23 21:54:13 -0400

    proud of myself

---
+++ python101/small/square.py
@@ -0,0 +1,16 @@
+# 9. Print a Square
+# Print a 5x5 square of * characters. Example output:
+
+# $ python3 square.py
+# *****
+# *****
+# *****
+# *****
+# *****
+
+a = 1
+b = 1
+c = 1
+d = 1
+e = 1
+
