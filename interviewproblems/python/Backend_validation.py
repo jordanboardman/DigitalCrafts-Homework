b2f2946b2609e8f88519bb5771844de5f67711d5
Jordan <jordanboardman15@gmail.com>
2022-08-26 16:38:36 -0400

    8/26 save

---
+++ interviewproblems/python/Backend_validation.py
@@ -0,0 +1,19 @@
+# 1. create empty list of users.
+# 2. use js from forms? Use push or append to add a user to the list.
+# 3.. valid emails have to be gmail, hotmail, or yahoo.
+
+# listOfUsers = []
+
+name = input("What's your name? ")
+print("Hi", name)
+
+
+# def addEmail(prompt):
+
+
+
+# def addPhone():
+
+
+# def addZip():
+
